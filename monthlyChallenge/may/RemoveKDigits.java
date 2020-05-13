d89a0915027333b86dfaf23a5e59f1d07129dc5d
kartikesarwani <64747360+kartikesarwani@users.noreply.github.com>
2020-05-13 21:31:29 +0530

    Update RemoveKDigits.java

--- monthlyChallenge/may/RemoveKDigits.java
+++ monthlyChallenge/may/RemoveKDigits.java
@@ -38,3 +38,26 @@ class Solution {
         return new int[] {min, idx};
     }
 }
+/*
+class Solution {
+    public String removeKdigits(String num, int k) {
+        if (k == num.length()) return "0";
+        StringBuilder sb = new StringBuilder();
+        
+        for (char c : num.toCharArray()) {
+            while (k > 0 && sb.length() != 0 && sb.charAt(sb.length() - 1) > c) {
+                sb.deleteCharAt(sb.length() - 1);
+                k--;
+            }
+            if (sb.length() > 0 || c != '0') sb.append(c);
+        }
+        while (k > 0) {
+            sb.deleteCharAt(sb.length() - 1);
+            k--;
+        }
+        while (sb.length() != 0 && sb.charAt(0) == '0') sb.deleteCharAt(0);
+        
+        return sb.length() == 0? "0" : sb.toString();
+    }
+} 
+*/
